103979833b0ec126cda95b339937d66fd430265c
ali osmar <ali.osmar.chehade@gmail.com>
2026-04-15 21:15:34 -0300

    test | rejeitar senhas sem letra maiuscula - RED

--- _test_/ValidadorSenha.test.js
+++ _test_/ValidadorSenha.test.js
@@ -4,4 +4,8 @@ describe('ValidadorSenha', () => {
         test('deve se rejeitar senha com menos de 8 caracteres', () => {
             expect(ValidadorSenha.validar('Aa0@567')).toBe(false);
         });
+
+        test('deve se rejeitar senha sem letra maiúscula', () => {
+            expect(ValidadorSenha.validar('A1@5678')).toBe(false);
+        });
     });
