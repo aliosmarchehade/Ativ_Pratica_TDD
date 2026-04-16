8288525f5806a3794150665f6c031fe28aa2fcb6
ali osmar <ali.osmar.chehade@gmail.com>
2026-04-15 21:26:21 -0300

    refactor | separando validacoes em metodos

--- _test_/ValidadorSenha.test.js
+++ _test_/ValidadorSenha.test.js
@@ -2,10 +2,10 @@ const ValidadorSenha = require('../src/model/ValidadorSenha');
 describe('ValidadorSenha', () => {
 
         test('deve se rejeitar senha com menos de 8 caracteres', () => {
-            expect(ValidadorSenha.validar('Aa0@567')).toBe(false);
+            expect(ValidadorSenha.validar('0@567')).toBe(false);
         });
 
         test('deve se rejeitar senha sem letra maiúscula', () => {
-            expect(ValidadorSenha.validar('A1@5678')).toBe(true);
+            expect(ValidadorSenha.validar('A1a@5678')).toBe(true);
         });
     });
--- src/model/ValidadorSenha.js
+++ src/model/ValidadorSenha.js
@@ -1,10 +1,17 @@
 class ValidadorSenha {
+
     static validar(senha) {
+        return this.temTamanhoMinimo(senha) &&
+               this.temMaiuscula(senha);
+    }
+
+    static temTamanhoMinimo(senha) {
         const MIN_LENGTH = 8;
-        const possuiTamanhoMinimo = senha.length >= MIN_LENGTH;
-        const possuiMaiuscula = /[A-Z]/.test(senha);
-        
-        return possuiTamanhoMinimo && possuiMaiuscula;
+        return senha.length >= MIN_LENGTH;
+    }
+
+    static temMaiuscula(senha) {
+        return /[A-Z]/.test(senha);
     }
 }
 
